5849b85fb8c45dc505c00559a47c56ddb3330f67
Tombo <anisimov.dany@mail.ru>
2023-04-11 00:46:39 +0300

    Gmail smtp data

--- app/app/settings.py
+++ app/app/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIT_PORT")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
